5f378c9621d99d783a5ea75518cf0bf262212ac6
balaguru <121935423+balaguru8072@users.noreply.github.com>
2024-06-24 17:15:20 +0530

    first commit

---
+++ Asstes/Javascript.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('nav a', 'botton');
+
+    links.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            scrollToSlowly(targetElement);
+        });
+    });
+
+    function scrollToSlowly(target) {
+        const startY = window.scrollY;
+        const endY = target.getBoundingClientRect().top + window.scrollY;
+        const distance = endY - startY;
+        const duration = 2000; // duration in milliseconds
+        let startTime = null;
+
+        function animation(currentTime) {
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startY, distance, duration);
+            window.scrollTo(0, run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        }
+
+        function ease(t, b, c, d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        }
+
+        requestAnimationFrame(animation);
+    }
+});
